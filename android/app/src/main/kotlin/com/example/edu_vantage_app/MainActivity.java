50243b84e53c91fb08f0fe237f851e69f0e300c4
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-01-05 18:32:52 +0200

    Update MainActivity.java

--- android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.java
+++ android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.java
@@ -1,6 +1,6 @@
-package com.example.edu_vantage_app;
+package com.example.edu_vantage_app
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity
 
-public class MainActivity extends FlutterActivity {
+class MainActivity: FlutterActivity() {
 }
