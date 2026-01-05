fd207be1b9b3538530efe1da7d4ee1b5b67dcf47
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-01-05 13:22:21 +0200

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.kt
@@ -7,7 +7,7 @@ import io.flutter.embedding.android.FlutterActivity
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // هذا السطر هو المسؤول عن منع تصوير الشاشة وتسجيل الفيديو
+        // كود منع تصوير الشاشة
         window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
     }
 }
