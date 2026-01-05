9ea37d6025ca4c3df8f4608a71b546ab9efb56fc
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-01-05 17:01:32 +0200

    Update and rename MainActivity.kt to MainActivity.java

---
+++ android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.edu_vantage_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.edu_vantage_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
