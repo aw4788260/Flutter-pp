d8cbe1947da93955afefc66c9e3e1d59ecc087a6
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-01-05 16:18:35 +0200

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.kt
@@ -7,7 +7,7 @@ import io.flutter.embedding.android.FlutterActivity
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // تفعيل وضع الحماية لمنع تصوير الشاشة
+        // إضافة حماية الشاشة (منع التصوير)
         window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
     }
 }
