496b74fed2f3760014200d4d0f694f374b6e70d1
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-01-18 21:18:33 +0200

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/edu_vantage_app/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.example.edu_vantage_app
 
+import android.app.NotificationManager
+import android.content.Context
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onDestroy() {
+        // 1. استدعاء مدير الإشعارات
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        
+        // 2. حذف جميع الإشعارات الخاصة بالتطبيق فوراً عند الإغلاق
+        notificationManager.cancelAll()
+        
+        super.onDestroy()
+    }
+}
