bcbb470a5e72524d603904531d0d695eebcc0922
aw4788260 <111379398+aw4788260@users.noreply.github.com>
2026-01-24 20:23:23 +0200

    Update app_colors.dart

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // مطابقة لـ tailwind.config في ملف index.html
-  static const Color backgroundPrimary = Color(0xFF1F2933);   // bg-background-primary
-  static const Color backgroundSecondary = Color(0xFF242F3D); // bg-background-secondary
+  static const Color backgroundPrimary = Color(0xFF242624);   // bg-background-primary
+  static const Color backgroundSecondary = Color(0xFF343834); // bg-background-secondary
 
   static const Color accentYellow = Color(0xFFE1AD01); // text-accent-yellow
   static const Color accentOrange = Color(0xFFD35400); // text-accent-orange
